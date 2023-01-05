6fe3dec042ab67efdfd174704002ddaadccee30d
Nico Plyley <nico@luxwaymedia.com>
2023-01-04 22:58:27 -0800

    :sparkles: Created header component

---
+++ src/lib/components/header/index.svelte
@@ -0,0 +1,16 @@
+<script>
+	let className = '';
+	export { className as class };
+	export let background = '';
+</script>
+
+<div
+	style={`
+			background-image: linear-gradient(89.89deg, rgba(0, 0, 0, 0.78) 0.11%, rgba(0, 0, 0, 0.1) 99.91%), linear-gradient(0deg, rgba(0, 0, 0, 0.14), rgba(0, 0, 0, 0.14)), url("${background}");
+      background-size: cover;
+      background-position: center center;
+ `}
+	class="w-full pt-24 pb-12 {className}"
+>
+	<slot />
+</div>
