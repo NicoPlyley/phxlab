f7dd6adbc96f0a7bd3fe9b1677486b07dda35e04
Nico Plyley <nico@luxwaymedia.com>
2023-01-04 22:59:13 -0800

    :sparkles: Added data for home page

---
+++ src/routes/+page.js
@@ -0,0 +1,12 @@
+export function load() {
+	return {
+		header: {
+			title: 'Boost Your Online Presence with Our Professional Web Design and SEO Services',
+			description: 'Transform your business with our expert website design and SEO strategies.',
+			button: {
+				title: 'Start A Project',
+				link: '/contact'
+			}
+		}
+	};
+}
