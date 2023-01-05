38c14be91e81adad855e58816dd75f70e8bd06a3
Nico Plyley <nico@luxwaymedia.com>
2023-01-04 23:23:07 -0800

    :sparkles: Updated to text component

--- src/lib/blocks/home/Header.svelte
+++ src/lib/blocks/home/Header.svelte
@@ -1,13 +1,18 @@
 <script>
-	import { Button, Container, Header, imageHandler } from '$lib';
+	import { Button, Container, Header, imageHandler, Text } from '$lib';
 
 	export let data;
 </script>
 
-<Header background={imageHandler('header-desert.webp', true)}>
+<Header
+	class="pt-32 pb-20 md:h-[600px] lg:h-[800px]"
+	background={imageHandler('header-desert.webp', true)}
+>
 	<Container class="flex h-full flex-col items-start justify-center text-center">
-		<h1 class="text-2xl font-bold text-white">{data.title}</h1>
-		<p class="mt-2.5 mb-5 text-base text-white">{data.description}</p>
+		<Text type="h1" class="w-full text-white md:text-4xl lg:text-6xl">{data.title}</Text>
+		<Text class="mt-2.5 mb-5 w-full text-white md:my-7 md:text-lg lg:text-2xl"
+			>{data.description}</Text
+		>
 		<Button class="mx-auto w-auto" link={data.button.link}>
 			{data.button.title}
 		</Button>
