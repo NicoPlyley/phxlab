ecd01123b35f5e934d016277f12fdead6f58d824
Nico Plyley <nico@luxwaymedia.com>
2023-01-04 16:39:14 -0800

    :sparkles: Created initial nav component

---
+++ src/lib/components/navigation/index.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Container, Logo } from '$lib';
+	import { Hamburger } from 'svelte-hamburgers';
+
+	let open;
+</script>
+
+<nav class="fixed top-0 h-20 w-full bg-primary">
+	<Container class="my-auto flex h-full items-center justify-between">
+		<div class="flex items-center">
+			<Logo class="h-14" />
+			<p class="ml-2.5 font-bold leading-4 text-white">
+				Phoenix<br />
+				Tech Lab
+			</p>
+		</div>
+		<Hamburger bind:open --color="white" />
+	</Container>
+</nav>
