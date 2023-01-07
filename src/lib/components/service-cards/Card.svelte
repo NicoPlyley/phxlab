5c17fe8290f524e85b5a892a771b5abfa3f74584
Nico Plyley <nico@luxwaymedia.com>
2023-01-06 18:15:51 -0800

    :sparkles: Created initial cards

---
+++ src/lib/components/service-cards/Card.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { Text } from '$lib';
+
+	export let title;
+	export let description;
+	export let icon;
+	export let idx;
+</script>
+
+<div class="flex w-1/4 flex-col items-start justify-between rounded-2xl bg-white px-7 py-4">
+	<div class="h-1/4 w-full">
+		<div
+			class="m-auto flex h-20 w-20 items-center justify-center rounded-3xl {idx % 2 !== 0
+				? 'bg-primary/10'
+				: 'bg-secondary/10'}"
+		>
+			{@html icon}
+		</div>
+	</div>
+	<div class="h-3/4 pt-10">
+		<Text type="h3" class="text-lg">{title}</Text>
+		<Text class="text-sm">{description}</Text>
+	</div>
+</div>
---
+++ src/lib/components/service-cards/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Card from './Card.svelte';
+
+	export let data;
+</script>
+
+<div class="flex w-full justify-evenly gap-[20px] py-20">
+	{#each data as item, idx}
+		<Card {...item} {idx} />
+	{/each}
+</div>
