03d633177a15b9144ba43e49b6d2c755dfc7e50d
Nico Plyley <nico@luxwaymedia.com>
2023-01-06 16:04:35 -0800

    :lipstick: Added color option to buttons

--- src/lib/components/button/index.svelte
+++ src/lib/components/button/index.svelte
@@ -1,12 +1,32 @@
 <script>
 	export let link = '';
 	export let blank = false;
+	export let white = false;
 	let className = '';
 	export { className as class };
 </script>
 
 {#if blank}
-	<a href={link} target="_blank" rel="noreferrer" class="button"><slot /></a>
+	<a
+		href={link}
+		class="inline-block rounded-full py-3 px-6 font-bold {className} {white
+			? 'bg-white'
+			: 'button'}"
+		target="_blank"
+		rel="noreferrer"><slot /></a
+	>
 {:else}
-	<a href={link} class="rounded-full bg-white py-3 px-6 font-bold {className}"><slot /></a>
+	<a
+		href={link}
+		class="inline-block rounded-full py-3 px-6 font-bold {className} {white
+			? 'bg-white'
+			: 'button'}"><slot /></a
+	>
 {/if}
+
+<style>
+	.button {
+		background: linear-gradient(89.62deg, #2faa9b 0.3%, #a2b747 99.67%);
+		border-radius: 30px;
+	}
+</style>
