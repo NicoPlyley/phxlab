36da43d700c0f8fa56715518759228c5e17b661b
Nico Plyley <nico@luxwaymedia.com>
2023-01-06 18:16:01 -0800

    :sparkles: Created initial background

---
+++ src/lib/components/background/index.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let image = '';
+	let className = '';
+	export { className as class };
+</script>
+
+<div
+	class={className}
+	style={`
+			background-image: linear-gradient(90deg, rgba(0, 0, 0, 0.2) 100%, rgba(0, 0, 0, 0.2) 100%), url("${image}");
+      background-size: cover;
+      background-position: center center;
+ `}
+>
+	<slot />
+</div>
