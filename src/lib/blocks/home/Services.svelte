87cebf06f9852fbb74fbc5d7aa23ab458be89bca
Nico Plyley <nico@luxwaymedia.com>
2023-01-06 18:16:11 -0800

    :sparkles: Created initial service section

---
+++ src/lib/blocks/home/Services.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Background, Container, imageHandler, ServiceCards, Text } from '$lib';
+
+	export let data;
+</script>
+
+<Background class="w-full" image={imageHandler('horseshoe-bend.webp', true)}>
+	<Container class="py-7 text-center md:py-20">
+		<Text class="text-white" type="h2">{data.title}</Text>
+		<ServiceCards data={data.cards} />
+	</Container>
+</Background>
