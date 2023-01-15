7e4a4d762a13b9ed99e11c9670248542f6ab3624
Nico Plyley <nico@luxwaymedia.com>
2023-01-15 08:42:34 -0500

    :sparkles: Added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
-  import '../app.css';
-  import {Navigation} from '$lib';
+	import '../app.css';
+	import { Footer, Navigation } from '$lib';
+
+	export let data = {};
+	let { footer } = data;
 </script>
 
 <Navigation />
 <slot />
+<Footer data={footer} />
