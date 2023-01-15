03419b11ab4a88f8d5216e86cd214254660e62f2
Nico Plyley <nico@luxwaymedia.com>
2023-01-15 08:42:09 -0500

    :bug: Fixed color issue

--- src/lib/blocks/home/Header.svelte
+++ src/lib/blocks/home/Header.svelte
@@ -9,7 +9,7 @@
 	background={imageHandler('header-desert.webp', true)}
 >
 	<Container class="flex h-full flex-col items-start justify-center text-center">
-		<Text type="h1" class="w-full text-white md:text-4xl lg:text-6xl">{data.title}</Text>
+		<Text type="h1" color class="w-full text-white md:text-4xl lg:text-6xl">{data.title}</Text>
 		<Text class="mt-2.5 mb-5 w-full text-white md:my-7 md:text-lg lg:text-2xl"
 			>{data.description}</Text
 		>
