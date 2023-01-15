7fe6b50d41cf22764a542f1f8e68cb81238ebd82
Nico Plyley <nico@luxwaymedia.com>
2023-01-15 08:41:08 -0500

    :bug: Fixed color issue

--- src/lib/components/text/index.svelte
+++ src/lib/components/text/index.svelte
@@ -2,34 +2,35 @@
 	export let type = 'p';
 	let className = '';
 	export { className as class };
+	export let color = null;
 </script>
 
 {#if type === 'h1'}
-	<h1 class="font-main text-2xl font-bold md:text-4xl {className}">
+	<h1 class="font-main text-2xl font-bold {!color && 'text-heading'} md:text-4xl {className}">
 		<slot />
 	</h1>
 {:else if type === 'h2'}
-	<h2 class="font-main text-2xl font-bold md:text-4xl {className}">
+	<h2 class="font-main text-2xl font-bold {!color && 'text-heading'} md:text-4xl {className}">
 		<slot />
 	</h2>
 {:else if type === 'h3'}
-	<h3 class="font-main font-bold {className}">
+	<h3 class="font-main text-xl font-bold {!color && 'text-heading'} {className}">
 		<slot />
 	</h3>
 {:else if type === 'h4'}
-	<h4 class="font-main font-bold {className}">
+	<h4 class="font-main font-bold {!color && 'text-heading'} {className}">
 		<slot />
 	</h4>
 {:else if type === 'h5'}
-	<h5 class="font-main font-bold {className}">
+	<h5 class="font-main font-bold {!color && 'text-heading'} {className}">
 		<slot />
 	</h5>
 {:else if type === 'h6'}
-	<h6 class="font-main font-bold {className}">
+	<h6 class="font-main font-bold {!color && 'text-heading'} {className}">
 		<slot />
 	</h6>
 {:else}
-	<p class="font-main text-base {className}">
+	<p class="font-main text-base {!color && 'text-text'} {className}">
 		<slot />
 	</p>
 {/if}
