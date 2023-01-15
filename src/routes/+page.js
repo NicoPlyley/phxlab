9f3567da8eb620e78064ac2728acdd6787a2c00e
Nico Plyley <nico@luxwaymedia.com>
2023-01-15 10:37:07 -0500

    :speech_balloon: Updated text

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -17,7 +17,7 @@ export function load() {
 			description:
 				"Phoenix Tech Lab, was founded in 2012 with the goal of providing top-quality web design services to businesses and individuals. Since then, we have grown into a team of skilled designers and developers who are passionate about creating visually stunning and user-friendly websites. <br /> <br /> We pride ourselves on our ability to understand our clients' needs and goals, and we work closely with them to create custom solutions that meet their specific requirements. Whether you are a small business looking to establish an online presence or a large corporation in need of a comprehensive rebranding, we have the expertise and experience to help you succeed.",
 			button: {
-				title: 'Learn More',
+				title: 'Discover Phoenix Tech Lab',
 				link: '/about'
 			}
 		},
