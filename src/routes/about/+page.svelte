16ecda7a650fd3a54f79a26f0f2a54ddb29935ed
Nico Plyley <nico@luxwaymedia.com>
2023-01-18 18:07:49 -0500

    :sparkles: Added header block and seo to page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Header } from '$lib/blocks/about';
+
+	export let data = {};
+	let { seo, header } = data;
+</script>
+
+<svelte:head>
+	<title>{seo.title}</title>
+	<meta name="description" content={seo.description} />
+</svelte:head>
+<Header data={header} />
