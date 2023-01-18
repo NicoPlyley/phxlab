21dfc022d45efd6a10e7b7c01f59cd1065a1171a
Nico Plyley <nico@luxwaymedia.com>
2023-01-18 18:06:33 -0500

    :sparkles: Created seo and header data

---
+++ src/routes/about/+page.js
@@ -0,0 +1,17 @@
+export function load() {
+	return {
+		seo: {
+			title: 'Discover Phoenix Tech Lab | Web Development & SEO Agency',
+			description:
+				'Discover the team, mission and services behind Phoenix Tech Lab, the web development and SEO agency that specializes in rebuilding websites and rising from the ashes to a stronger online presence. Learn more about us and how we can help your business succeed.'
+		},
+		header: {
+			title: 'Discover Phoenix Tech Lab',
+			subtitle: 'Web Development and SEO Experts in Revitalizing Websites',
+			image: {
+				path: 'header-desert.webp',
+				dev: true
+			}
+		}
+	};
+}
