3582ecc50551c6dc2e3123f2bd3f10693f782ee7
Nico Plyley <nico@luxwaymedia.com>
2023-01-18 18:06:53 -0500

    :sparkles: Created page header component

---
+++ src/lib/components/page-header/index.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Container, Header, Text } from '$lib';
+
+	export let image = '';
+	export let data = {};
+	let { title, subtitle } = data;
+</script>
+
+<Header class="h-[500px]" background={image}>
+	<Container class="flex h-full items-center justify-center">
+		<Text type="h1" color class="text-center text-white">
+			{title}
+			{#if subtitle}
+				<span class="block text-2xl font-normal">{subtitle}</span>
+			{/if}
+		</Text>
+	</Container>
+</Header>
