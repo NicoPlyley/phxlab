fcd13b2c6a7c6c6d610ac9e73ea49e006c877e79
Nico Plyley <nico@luxwaymedia.com>
2023-01-18 21:31:05 -0500

    :sparkles: Added about section info

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -12,6 +12,20 @@ export function load() {
 				path: 'header-desert.webp',
 				dev: true
 			}
+		},
+		about: {
+			title: 'About Us',
+			description: [
+				'We are a web development and SEO agency that specializes in rebuilding websites and rising from the ashes to a stronger online presence. Our team of experts will design and optimize a stunning website that will help you stand out online and increase your visibility on search engines.',
+				'Our company began in 2012 under the name LuxWay, where we started as a video production company. However, as the digital landscape evolved, in 2015 we saw an opportunity to expand our services to include website development. In 2022, we rebranded to Phoenix Tech Lab to reflect our new focus and mission.',
+				'Our agency was founded by Nico Plyley and Jonathan Nyquist, who bring a wealth of experience and expertise in website development and SEO. Together, they have led Phoenix Tech Lab to become a leading agency in the industry.',
+				'We pride ourselves on our ability to understand the unique needs of each of our clients and to deliver customized solutions that drive results. Our team of web developers and SEO experts are dedicated to creating websites that are not only visually stunning but also optimized for search engines, resulting in a better online visibility for our clients.',
+				'We invite you to take a look at our portfolio to see the amazing websites and SEO campaigns we have created for our clients. If you are looking to revitalize your website and improve your online presence, contact us today to schedule a consultation.'
+			],
+			image: {
+				path: 'owners.webp',
+				dev: true
+			}
 		}
 	};
 }
