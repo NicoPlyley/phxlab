accb8111df97efff53c021aa52c4ae5486264865
Nico Plyley <nico@luxwaymedia.com>
2023-01-18 22:06:39 -0500

    :sparkles: Created team component

---
+++ src/lib/components/team-cards/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { tw } from '$lib';
+	import Card from './Card.svelte';
+
+	export let data = {};
+</script>
+
+<div class={tw('mt-7 flex flex-wrap justify-evenly', 'md')}>
+	{#each data as person}
+		<Card {person} />
+	{/each}
+</div>
