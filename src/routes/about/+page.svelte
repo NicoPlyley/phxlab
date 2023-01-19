84e5f4f743adfa12032bff318554cbeb3bd055d7
Nico Plyley <nico@luxwaymedia.com>
2023-01-18 21:31:13 -0500

    :sparkles: Added about section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,15 @@
 <script>
-	import { Header } from '$lib/blocks/about';
+	import { About, Header } from '$lib/blocks/about';
 
 	export let data = {};
-	let { seo, header } = data;
+	let { seo, header, about } = data;
 </script>
 
 <svelte:head>
 	<title>{seo.title}</title>
 	<meta name="description" content={seo.description} />
 </svelte:head>
+
 <Header data={header} />
+
+<About data={about} />
