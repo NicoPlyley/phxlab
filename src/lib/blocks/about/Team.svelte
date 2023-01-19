0eb23a83c88fb5b386b5563abb1f87962a406633
Nico Plyley <nico@luxwaymedia.com>
2023-01-18 22:06:50 -0500

    :sparkles: Created team block

---
+++ src/lib/blocks/about/Team.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Container, TeamCards, Text } from '$lib';
+
+	export let data = {};
+</script>
+
+<div class="my-7 bg-primary py-10">
+	<Container>
+		<Text type="h2" color class="text-center text-white">
+			{data.title}
+		</Text>
+		<TeamCards data={data.people} />
+	</Container>
+</div>
