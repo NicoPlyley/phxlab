70b5058ae844112b7a981be395422db7e6276db2
Nico Plyley <nico@luxwaymedia.com>
2023-01-18 22:37:12 -0500

    :lipstick: Added margins

--- src/lib/blocks/about/Team.svelte
+++ src/lib/blocks/about/Team.svelte
@@ -4,7 +4,7 @@
 	export let data = {};
 </script>
 
-<div class="my-7 bg-primary py-10">
+<div class="my-7 mb-14 bg-primary py-10">
 	<Container>
 		<Text type="h2" color class="text-center text-white">
 			{data.title}
