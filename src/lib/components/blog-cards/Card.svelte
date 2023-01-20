a32387eeefab3b601c00a86480ffdedc178bf507
Nico Plyley <nico@luxwaymedia.com>
2023-01-20 01:28:26 -0500

    :sparkles: Created blog cards component

---
+++ src/lib/components/blog-cards/Card.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { Button, imageHandler, Text } from '$lib';
+
+	export let data = {};
+
+  let { title, date, image } = data;
+
+  date = new Date(date).toLocaleDateString('en-us', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+</script>
+
+<div class="overflow-hidden rounded-2xl bg-white">
+  <img alt={title}
+       src={imageHandler(image.path, true)}
+  />
+  <div class="py-4 px-3">
+    <Text>{date.toLocaleString()}</Text>
+    <Text class="py-4"
+          type="h3"
+    >
+      {title}
+    </Text>
+    <Button class="text-white">Read Post</Button>
+  </div>
+</div>
---
+++ src/lib/components/blog-cards/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Card from './Card.svelte';
+
+	export let data = {};
+</script>
+
+<div class="flex justify-evenly gap-20">
+  {#each data as post}
+    <Card data={post} />
+  {/each}
+</div>
