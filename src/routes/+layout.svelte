caed265211c6b5ecd45958d0c2cc83d6590c77ef
Nico Plyley <nico@luxwaymedia.com>
2023-01-21 05:09:31 -0500

    :sparkles: Added info to store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script>
   import '../app.css';
-  import { Footer, Navigation } from '$lib';
+  import { contactInfoStore, Footer, Navigation } from '$lib';
 
   export let data = {};
   let { footer, navigation } = data;
+
+  contactInfoStore.set({
+    social: [ ...footer.info.social ],
+    contact: [ ...footer.lists[2].items ],
+  });
 </script>
 
 <Navigation data={navigation} />
