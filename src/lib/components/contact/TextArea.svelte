3825812d146976da3a1f257043aa29843ce6a371
Nico Plyley <nico@luxwaymedia.com>
2023-01-21 05:10:16 -0500

    :sparkles: Created contact component

---
+++ src/lib/components/contact/TextArea.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let placeholder = '';
+</script>
+
+<textarea class="bg-tertiary-90 px-7 py-4 rounded-lg w-full h-32 resize-none"
+          {placeholder}
+/>
+
+<style>
+    ::placeholder {
+        color: #848484;
+    }
+
+    :-ms-input-placeholder {
+        color: #848484;
+    }
+
+    ::-ms-input-placeholder {
+        color: #848484;
+    }
+</style>
