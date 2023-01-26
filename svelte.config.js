e6f44699ecdc9871273ccb29c283695fad6b9c6c
Nico Plyley <nico@luxwaymedia.com>
2023-01-25 20:10:04 -0500

    :sparkles: Updated to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 // noinspection JSUnusedGlobalSymbols
 
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 const config = {
