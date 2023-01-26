777c20f9d9014433bfdca22421ca3fdd4091f72a
Nico Plyley <nico@luxwaymedia.com>
2023-01-25 19:48:47 -0500

    :sparkles: Created privacy policy

---
+++ src/routes/privacy/+page.js
@@ -0,0 +1,18 @@
+export let load;
+load = async () => {
+  return {
+    seo: {
+      title: 'Privacy Policy | Phoenix Tech Lab',
+      description:
+        'At Phoenix Tech Lab, we take your privacy seriously. Learn more about how we collect, use, and protect your personal information in our Privacy Policy.',
+    },
+    header: {
+      title: 'Privacy Policy',
+      subtitle: 'Our Commitment to Keeping Your Information Secure',
+      image: {
+        path: 'cactus-bg2.webp',
+        dev: true,
+      },
+    },
+  };
+};
