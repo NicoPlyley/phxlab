ec04b190179bf1ed3ba62dd69a35647958e4eacc
Nico Plyley <nico@luxwaymedia.com>
2023-02-05 09:26:54 -0500

    :sparkles: Created blocks for page

---
+++ src/lib/blocks/contact/Form.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Contact, Container, imageHandler } from '$lib';
+</script>
+
+<div class="relative py-32">
+  <div class="absolute top-16 w-full h-full -z-10"
+       style={`
+     background-image: url('${imageHandler('/contact/contact-bg.svg')}');
+     background-position: bottom;
+     background-size: cover;
+     `}
+  />
+
+  <Container>
+    <Contact />
+  </Container>
+</div>
---
+++ src/lib/blocks/contact/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { imageHandler, PageHeader } from '$lib';
+
+  export let data = {};
+</script>
+
+<PageHeader {data}
+            image={imageHandler(data.image.path, data.image.dev)}
+/>
