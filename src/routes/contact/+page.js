6b6a724390ab52607eb8ee745785b7d93f412d2e
Nico Plyley <nico@luxwaymedia.com>
2023-02-05 09:26:41 -0500

    :sparkles: Created server page

---
+++ src/routes/contact/+page.js
@@ -0,0 +1,18 @@
+export let load;
+load = () => {
+  return {
+    seo: {
+      title: 'Contact Phoenix Tech Lab | Web Development & SEO Agency',
+      description:
+        'Get in touch with Phoenix Tech Lab, the leading web development & SEO agency. Contact us to learn how we can help elevate your online presence today.',
+    },
+    header: {
+      title: 'Get in Touch',
+      subtitle: 'Your Trusted Web Development & SEO Partner',
+      image: {
+        path: 'cactus-bg2.webp',
+        dev: true,
+      },
+    },
+  };
+};
